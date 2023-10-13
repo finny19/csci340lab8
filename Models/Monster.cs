423ba45f0cfbd31ee5c247fdadd2d7e3ac268814
Fin Lockert <fin@Fins-MacBook-Pro.local>
2023-10-12 22:43:56 -0500

    new model

---
+++ Models/Monster.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace csci340lab8.Models;
+
+public class Monster
+{
+    public int Id { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(30)]
+    public string Type { get; set; } = string.Empty;
+
+    [Range(1, 30)]
+    public decimal Level { get; set; }
+}
